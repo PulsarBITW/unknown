18bf27278728c77010043d4d48401f12e6062d53
PulsarBITW <GordeyBITW@gmail.com>
2025-04-19 21:37:15 +0300

    format

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { allSettled } from "effector";
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import {allSettled} from 'effector';
 
-import { appScope } from "@shared/config";
+import {appScope} from '@shared/config';
 
-import { appStarted } from "./model";
-import { AppWithProvider } from "./ui";
+import {appStarted} from './model';
+import {AppWithProvider} from './ui';
 
 async function initializeApp() {
-  const root = document.getElementById("root")!;
+  const root = document.getElementById('root')!;
 
-  await allSettled(appStarted, { scope: appScope });
+  await allSettled(appStarted, {scope: appScope});
 
   createRoot(root).render(
     <StrictMode>
