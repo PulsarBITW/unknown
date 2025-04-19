2b0a5df5f2e4a2a9f6a589e0d67b07ed3a4e177a
PulsarBITW <GordeyBITW@gmail.com>
2025-04-19 17:25:49 +0300

    change vite config, add stylelint, add alias

---
+++ frontend/stylelint.config.js
@@ -0,0 +1,10 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: "stylelint-config-standard",
+  plugins: ["stylelint-prettier", "stylelint-order"],
+  rules: {
+    "prettier/prettier": true,
+    "order/properties-alphabetical-order": true,
+    "block-no-empty": true,
+  },
+};
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,50 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        exportType: "default",
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: "**/*.svg",
+    }),
+    checker({
+      typescript: { tsconfigPath: "tsconfig.app.json" },
+      eslint: { lintCommand: "eslint .", useFlatConfig: true },
+      stylelint: {
+        lintCommand: 'stylelint "**/*.{css,scss}"',
+      },
+      overlay: {
+        position: "tl",
+      },
+    }),
+  ],
+  esbuild: { legalComments: "none" },
+  css: {
+    modules: {
+      generateScopedName: "[local]__[hash:base64:5]",
+    },
+  },
+  server: {
+    port: 3001,
+  },
+  preview: {},
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "src/app"),
+      "@entities": path.resolve(__dirname, "src/entities"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@widgets": path.resolve(__dirname, "src/widgets"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+    },
+  },
 });
