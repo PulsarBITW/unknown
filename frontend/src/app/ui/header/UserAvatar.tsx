8024eba62085bc7280df04829a1976633bbc4c6f
PulsarBITW <GordeyBITW@gmail.com>
2025-04-29 21:39:34 +0300

    Enhance header component and styling
    - Updated header component to use a logo link for navigation.
    - Introduced a new navigation list style for better layout.
    - Replaced static links with buttons for improved user interaction.
    - Added user authentication handling to display user avatar or login button based on authentication state.

---
+++ frontend/src/app/ui/header/UserAvatar.tsx
@@ -0,0 +1,34 @@
+import {Avatar, Button, Flex, Popover, Separator, Text} from '@radix-ui/themes';
+
+import {ModelUserResponseDto} from '@shared/api';
+
+interface UserAvatarProps {
+  currentUser: ModelUserResponseDto;
+  logout: () => void;
+}
+
+export const UserAvatar = ({currentUser, logout}: UserAvatarProps) => {
+  const fullName = currentUser ? `${currentUser.firstName} ${currentUser.lastName}` : '';
+
+  return (
+    <Popover.Root>
+      <Popover.Trigger>
+        <Avatar
+          alt="User Avatar"
+          size="3"
+          fallback={<span>You</span>}
+          src="https://github.com/shadcn.png"
+        />
+      </Popover.Trigger>
+      <Popover.Content>
+        <Flex gap="2" direction="column">
+          <Text>{fullName}</Text>
+          <Separator size="4" />
+          <Button color="red" onClick={logout}>
+            Logout
+          </Button>
+        </Flex>
+      </Popover.Content>
+    </Popover.Root>
+  );
+};
--- frontend/src/app/ui/header/index.tsx
+++ frontend/src/app/ui/header/index.tsx
@@ -1,40 +1,55 @@
-import {Avatar, Flex, Text} from '@radix-ui/themes';
+import {Button, Flex, Text} from '@radix-ui/themes';
 import {Link} from '@tanstack/react-router';
+import {useUnit} from 'effector-react';
 
 import {ROUTES} from '@shared/config';
+
+import {currentUserModel} from '@entities/current-user';
+
+import {authModel} from '@features/auth';
+
+import {UserAvatar} from './UserAvatar';
 
 import styles from './header.module.css';
 
 export const Header = () => {
+  const [isAuthenticated, logout, currentUser] = useUnit([
+    authModel.$isAuth,
+    authModel.logout,
+    currentUserModel.$currentUser,
+  ]);
+
   return (
     <header className={styles.header}>
       <Flex justify="between" align="center" px="4" py="3">
-        <Text size="4" weight="bold" color="indigo">
-          MyApp
-        </Text>
+        <Link to={ROUTES.home.absolutePath} className={styles.logo}>
+          <Text size="4" weight="bold" color="indigo">
+            MyApp
+          </Text>
+        </Link>
 
         <nav>
-          <ul>
+          <ul className={styles['nav-list']}>
             <li>
-              <Link to={ROUTES.home.absolutePath}>Home</Link>
+              <Button asChild variant="soft">
+                <Link to={ROUTES.home.absolutePath}>Home</Link>
+              </Button>
             </li>
             <li>
-              <Link to={ROUTES.about.absolutePath}>About</Link>
-            </li>
-            <li>
-              <Link to={ROUTES.auth.children.login.absolutePath}>Login</Link>
+              <Button asChild variant="soft">
+                <Link to={ROUTES.about.absolutePath}>About</Link>
+              </Button>
             </li>
           </ul>
         </nav>
 
-        <Flex align="center" gap="2">
-          <Avatar
-            alt="User Avatar"
-            size="3"
-            fallback={<span>You</span>}
-            src="https://github.com/shadcn.png"
-          />
-        </Flex>
+        {isAuthenticated && currentUser !== null ? (
+          <UserAvatar currentUser={currentUser} logout={logout} />
+        ) : (
+          <Button asChild color="blue">
+            <Link to={ROUTES.auth.children.login.absolutePath}>Login</Link>
+          </Button>
+        )}
       </Flex>
     </header>
   );
